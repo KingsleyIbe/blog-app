2feb6db022149c5ee8906b068140245e8004bb20
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-03-29 12:41:53 +0100

    Add model classess

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_26_083103) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_29_092534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_26_083103) do
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "commentts", force: :cascade do |t|
+    t.integer "authorId"
+    t.integer "postId"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "likes", force: :cascade do |t|
