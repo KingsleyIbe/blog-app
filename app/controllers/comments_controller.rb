5e921bf65f3cf0331644f1776cd3f1d6c7946d41
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-04-01 11:19:43 +0100

    Make changes to file as recommended by the reviewer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,10 @@
 class CommentsController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])
-    new_comment = current_user.comments.new(
-      text: comment_params,
-      user_id: current_user.id,
-      post_id: @post.id
-    )
-    new_comment.update_comments_counter
+    @comment = current_user.comments.new(params)
+    @comment.post_id = params[:post_id]
+
     if new_comment.save
+      new_comment.update_comments_counter
       redirect_to "/users/#{@post.user_id}/posts/#{@post.id}", notice: 'Success!'
     else
       render :new, alert: 'Error occured!'
